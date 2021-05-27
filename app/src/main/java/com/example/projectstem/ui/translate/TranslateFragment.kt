f5d14b204afb6d76126405a9a6309c634fc79757
Stepan8888 <58593274+spronchev@users.noreply.github.com>
2021-05-27 23:32:31 +0300

    Added library fragment and translation

--- app/src/main/java/com/example/projectstem/ui/translate/TranslateFragment.kt
+++ app/src/main/java/com/example/projectstem/ui/translate/TranslateFragment.kt
@@ -1,14 +1,19 @@
 package com.example.projectstem.ui.translate
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.projectstem.R
 import com.example.projectstem.databinding.FragmentTranslateBinding
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.translate.TranslateLanguage.*
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.TranslatorOptions
 
 class TranslateFragment : Fragment() {
 
@@ -23,7 +28,7 @@ class TranslateFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         notificationsViewModel =
             ViewModelProvider(this).get(TranslateViewModel::class.java)
 
@@ -31,12 +36,78 @@ class TranslateFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.textTranslate
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+
+        notificationsViewModel.text.observe(viewLifecycleOwner, {
             textView.text = it
         })
+        //Drop down menu of languages - From
+        val spinnerFrom: Spinner = root.findViewById(R.id.spinnerFrom)
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.lCategory,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerFrom.adapter = adapter
+        }
+        //Drop down menu of languages - To
+        val spinnerTo: Spinner = root.findViewById(R.id.spinnerTo)
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.lCategory,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerTo.adapter = adapter
+        }
+
+        val button = root.findViewById<Button>(R.id.buttonTranslate)
+        val translateFromField = root.findViewById<EditText>(R.id.translateFrom)
+        val translateToField = root.findViewById<EditText>(R.id.translateTo)
+        button.setOnClickListener {
+            val translateFromLanguage = spinnerFrom.selectedItem.toString()
+            val translateToLanguage = spinnerTo.selectedItem.toString()
+            val wordToBeTranslated = translateFromField.text.toString()
+            val options = TranslatorOptions.Builder()
+                .setSourceLanguage(getLanguageFromDropDownMenu(translateFromLanguage))
+                .setTargetLanguage(getLanguageFromDropDownMenu(translateToLanguage))
+                .build()
+            val translator = Translation.getClient(options)
+            val conditions = DownloadConditions.Builder()
+                .requireWifi()
+                .build()
+            translator.downloadModelIfNeeded(conditions)
+                .addOnSuccessListener {
+                    //Success downloading model. Continue to translation
+                    translator.translate(wordToBeTranslated)
+                        .addOnSuccessListener { translatedText ->
+                            translateToField.setText(translatedText)
+                        }
+                        .addOnFailureListener { exception ->
+                            Log.e("s", Log.getStackTraceString(exception))
+                        }
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("s", Log.getStackTraceString(exception))
+                }
+        }
         return root
     }
 
+    private fun getLanguageFromDropDownMenu(language: String): String {
+        when (language) {
+            "Afrikaans" -> {
+                return AFRIKAANS
+            }
+            "Arabic" -> {
+                return ARABIC
+            }
+            "Belarusian" -> {
+                return BELARUSIAN
+            }
+        }
+        return "Language not found"
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
