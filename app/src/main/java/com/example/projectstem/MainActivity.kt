1407d7bb8e0d6bc8763f27c8fcf48bb78ca9bc69
ViragSzabo <58694341+ViragSzabo@users.noreply.github.com>
2021-05-30 01:35:24 +0200

    (Have done it twice.) Trying to study from tutorials. I able to make the button and start a function in the MainActivity.kt where it is called a setClickListener for clicking on the button.  For making new translation from one language to another boxes are still in progress.

--- app/src/main/java/com/example/projectstem/MainActivity.kt
+++ app/src/main/java/com/example/projectstem/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.projectstem
 
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -31,5 +32,10 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //fragment_library: click on the button
+        button.setOnClickListener{
+            //
+        }
     }
 }
