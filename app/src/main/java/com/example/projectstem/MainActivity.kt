363f3e6a5818653f4ab586060fe4674fe51cf59d
ViragSzabo <58694341+ViragSzabo@users.noreply.github.com>
2021-06-03 22:31:44 +0200

    Button clicking, errors fixing
    I tried to work on the add button without any error and/or warning messages after create the minigames menu. I ended up fixing a lot of errors I got after I started to build the program. (in Games- and Library Fragment, I put the error part in a comment and I fixed SmoothGreen to smoothGreen in listview_selector.
    
    (I still cannot test if my button is working and it gives the message after you click, so if someone could test it after he pulled this change, that would be lovely!)

--- app/src/main/java/com/example/projectstem/MainActivity.kt
+++ app/src/main/java/com/example/projectstem/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.projectstem
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -11,8 +8,11 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.projectstem.databinding.ActivityMainBinding
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -36,15 +36,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         navView.setupWithNavController(navController)
     }
 
-    //Fragment.libray: clicking on the button
-    protected fun onCreateByClick(savedValues: Bundle) {
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener(this)
-        byClick()
-    }
+    //FragmentLibrary: clicking on the button (onClickListener)
+    private val button = findViewById<Button>(R.id.button) as Button
+    val message = "You clicked on me!"
 
-    // Implement the OnClickListener callback
-    fun byClick() {
-        Toast.makeText(MainActivity.this, "You clicked on me!", Toast.LENGTH_SHORT).show()
+    fun getClicked(view: View) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
