524e5cb654bb2072313c9a530744e2dc4c5095ba
ViragSzabo <58694341+ViragSzabo@users.noreply.github.com>
2021-06-09 16:42:26 +0200

    Fixing the warnings.

--- app/src/main/java/com/example/projectstem/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/example/projectstem/ui/library/LibraryFragment.kt
@@ -6,18 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
-//import android.widget.TextView
-//import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-//import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.example.projectstem.R
-import com.example.projectstem.databinding.FragmentLibraryBinding
-import android.content.Intent
-import android.util.Log
-import androidx.fragment.app.activityViewModels
 
 class LibraryFragment : Fragment(){
 
@@ -37,7 +28,7 @@ class LibraryFragment : Fragment(){
         }
 
 
-        val buttonLang = view?.findViewById<Button>(R.id.button_lang)
+        val buttonLang = view.findViewById<Button>(R.id.button_lang)
         buttonLang?.setOnClickListener {
         Navigation.findNavController(view).navigate(R.id.navigation_library_hover)
         }
