1066d043a43377306003b5b969eca59b82e1740c
spronchev <58593274+spronchev@users.noreply.github.com>
2021-06-10 13:45:17 +0300

    Word parsing

--- app/src/main/java/com/example/projectstem/ui/library/LibraryCategoryFragment.kt
+++ app/src/main/java/com/example/projectstem/ui/library/LibraryCategoryFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.projectstem.R
+import androidx.navigation.Navigation
 
 class LibraryCategoryFragment : Fragment() {
 
@@ -20,7 +22,13 @@ class LibraryCategoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_library_category, container, false)
+        val view = inflater.inflate(R.layout.fragment_library_category, container, false)
+        val buttonAddWord = view?.findViewById<Button>(R.id.addBtn)
+        // TODO: Get languages from specific group and apply them
+        buttonAddWord?.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.navigation_translate)
+        }
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
