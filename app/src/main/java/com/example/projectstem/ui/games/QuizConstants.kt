bd5e5b8a5ae6a7b946da893496b5e3c55402b134
Aleksei Skorjak <43403562+AlekseiSkr@users.noreply.github.com>
2021-06-15 10:11:33 +0200

    Updated QuizGame to be more consistent

--- app/src/main/java/com/example/projectstem/ui/games/QuizConstants.kt
+++ app/src/main/java/com/example/projectstem/ui/games/QuizConstants.kt
@@ -1,5 +1,6 @@
 package com.example.projectstem.ui.games
 
+import android.util.Log
 import com.example.projectstem.model.Word
 import kotlin.random.Random
 
@@ -15,30 +16,38 @@ object QuizConstants {
     // END
     
     fun getQuestions(wordList: List<Word>): ArrayList<QuizQuestion> {
+
         val questionsList = ArrayList<QuizQuestion>()
 
         for(i in 1..wordList.size){
+            try {
+                var listOFWords = wordList
+                var id: Int = 1
+                var randomIndex = Random.nextInt(0, listOFWords.size)
+                val word = listOFWords[randomIndex]
+                var wordInQuestion: String = word.original
+                var correctAnwser: String = word.translation
+                var option1: String = if (listOFWords[randomIndex].translation != correctAnwser) {
+                    listOFWords[randomIndex].translation
+                } else {
+                    listOFWords!![randomIndex + 1].translation
+                }
+                var option2: String = listOFWords[randomIndex].translation
+                var option3: String = listOFWords[randomIndex].translation
 
-        var listOFWords = wordList
-        var id: Int = 1
-        var randomIndex = Random.nextInt(0, listOFWords.size)
-            val word = listOFWords[randomIndex]
-        var wordInQuestion: String = word.original
-        var correctAnwser: String = word.translation
-        var option1: String = if(listOFWords[randomIndex].translation != correctAnwser){ listOFWords[randomIndex].translation }else{ listOFWords!![randomIndex + 1].translation}
-        var option2: String = listOFWords[randomIndex].translation
-        var option3: String = listOFWords[randomIndex].translation
+                val newQuestion = QuizQuestion(id, "What does this word mean?", wordInQuestion, correctAnwser, option1, option2, option3, 1)
+                questionsList.add(newQuestion)
+                id++
+                Log.d("quizGaming", "$wordInQuestion, $correctAnwser, $option1, $option2, $option3")
 
-        //INITIALIZE VARS
+            }catch (e: Exception){
+                Log.d("quizGaming", "$e")
+            }
 
+                //INITIALIZE VARS
+                if (i == 6) break
+            }
 
-
-        val newQuestion = QuizQuestion(id, "What does this word mean?", wordInQuestion, correctAnwser, option1, option2, option3, 1)
-        questionsList.add(newQuestion)
-        id++
-            if (id == 5 || i == 6) break
-
-        }
         return questionsList
     }
 }
