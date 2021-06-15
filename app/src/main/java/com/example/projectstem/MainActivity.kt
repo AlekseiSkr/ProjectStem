8547bdeb0a0122ab2c44f608c58bfad4e31588f7
Aleksei Skorjak <43403562+AlekseiSkr@users.noreply.github.com>
2021-06-15 10:30:40 +0200

    Deleted depreciated user related code and functionality

--- app/src/main/java/com/example/projectstem/MainActivity.kt
+++ app/src/main/java/com/example/projectstem/MainActivity.kt
@@ -42,57 +42,6 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        createJsonFile();
     }
 
-    fun createJsonFile(){
-
-        val fileName = "stem_groups.json"
-        val path = this.getExternalFilesDir(null) //get the dir like Android/data/data/stem_package/
-        val letDirectory = File(path, "stem")
-        try{
-            letDirectory.mkdirs()
-            var fileObject = File(letDirectory, fileName)
-            // create a new file
-            val isNewFileCreated : Boolean = fileObject.createNewFile()
-            if(isNewFileCreated){
-                Log.i("FileStatus", "$fileName is created successfully on $fileObject")
-            } else {
-                Log.i("FileStatus", "$fileName already exists on $fileObject")
-            }
-
-        }catch (e : Exception) {
-            Log.e("creationCatch", "$e")
-        }
-    }
-
-    fun save(view: View) {
-        // Save txt file
-//        val path = this.getExternalFilesDir(null)
-//        val letDirectory = File(path, "stem")
-//        letDirectory.mkdirs()
-//        val file = File(letDirectory, "savedWords.txt")
-//        val str: String = getString(R.string.app_name)
-//
-//        FileOutputStream(file).bufferedWriter().use { it.write(str) }
-//        Toast.makeText(this, "Saved to " + path + "/" + file.name, Toast.LENGTH_LONG).show()
-
-        // Save json file
-        // Accessible files via View -> Tool Windows -> Device File Explorer (while running emulator) -> storage/emulated/0/Android/data/com.example.projectstem/files/stem
-        val array: Array<String> = resources.getStringArray(R.array.CategoryListOfWords)
-
-        val path = this.getExternalFilesDir(null)
-        val letDirectory = File(path, "stem")
-        letDirectory.mkdirs()
-        val file = File(letDirectory, "savedWords.json")
-
-        val gsonPretty = GsonBuilder().setPrettyPrinting().create()
-        val arrayToList = gsonPretty.toJson(array)
-
-        val arrayToPretty: String = gsonPretty.toJson(arrayToList)
-        file.writeText(arrayToPretty)
-
-        Toast.makeText(this, "Saved to " + path + "/" + file.name, Toast.LENGTH_LONG).show()
-
-    }
 }
