db48ad3d1dbab23343e80007b0f8ce0826dfcab1
ViragSzabo <58694341+ViragSzabo@users.noreply.github.com>
2021-06-21 11:01:10 +0200

    Added @constructor to the JavaDoc to the LibraryViewModel.kt

--- app/src/main/java/com/example/projectstem/ui/library/LibraryViewModel.kt
+++ app/src/main/java/com/example/projectstem/ui/library/LibraryViewModel.kt
@@ -6,6 +6,12 @@ import androidx.lifecycle.ViewModel
 
 class LibraryViewModel : ViewModel() {
 
+    /**
+     * This function write out the text on the page of the library
+     * MutableLiveData a class that extends the LiveData type class
+     * @constructor value includes a general string
+     * @return the text value
+     */
     private val _text = MutableLiveData<String>().apply {
         value = "This is library Fragment"
     }
