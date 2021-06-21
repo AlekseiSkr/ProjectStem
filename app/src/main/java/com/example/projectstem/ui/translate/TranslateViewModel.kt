ee35d8cbaaacde47a6d474f64991cf9b20965a64
ViragSzabo <58694341+ViragSzabo@users.noreply.github.com>
2021-06-21 11:00:10 +0200

    Added @constructor to the JavaDoc to the TranslateViewModel.kt

--- app/src/main/java/com/example/projectstem/ui/translate/TranslateViewModel.kt
+++ app/src/main/java/com/example/projectstem/ui/translate/TranslateViewModel.kt
@@ -6,6 +6,12 @@ import androidx.lifecycle.ViewModel
 
 class TranslateViewModel : ViewModel() {
 
+    /**
+     * This function write out the text on the page of the translate
+     * MutableLiveData a class that extends the LiveData type class
+     * @constructor value includes a general string
+     * @return the text value
+     */
     private val _text = MutableLiveData<String>().apply {
         value = "This is translate Fragment"
     }
