1d0e304517d9ad25e36319b247f46a88fcf79bf8
Jonas_R <Jonas>
2021-06-22 10:11:04 +0200

    Added LibraryFragment Comments

--- app/src/main/java/com/example/projectstem/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/example/projectstem/ui/library/LibraryFragment.kt
@@ -26,16 +26,24 @@ class LibraryFragment : Fragment(){
 
        val view = inflater.inflate(R.layout.fragment_library,container, false)
 
-
         val groups = view.findViewById<RecyclerView>(R.id.rvGroups)
         //button to go to hover page
 
+
+        /**
+         * onClick listener on the @buttonLang button in the fragment_library page to add a word.
+         */
         val buttonLang = view.findViewById<Button>(R.id.button_lang)
         buttonLang?.setOnClickListener {
         Navigation.findNavController(view).navigate(R.id.action_grp2hvr)
         }
 
         //recyclerview
+
+        /**
+         * supply the context and define the adapter which will have the configuration logic. The RecyclerView library dynamically creates the elements when they're needed.
+         * RecyclerView reuses the view for new items that have scrolled onscreen. This reuse vastly improves performance, improving the app's responsiveness and reducing power consumption.
+         */
         val gridLayout = GridLayoutManager(requireContext(),2)
         val adapter = GroupListAdapter()
         val recyclerView = view.findViewById<RecyclerView>(R.id.rvGroups)
@@ -43,6 +51,10 @@ class LibraryFragment : Fragment(){
         recyclerView.layoutManager = gridLayout
 
         // UserViewModel
+        /**
+         * groupViewModel follows the observer pattern. It notifies Observer objects when underlying data changes.
+         * It can consolidate the code to update the UI in these Observer objects. That way, we don't need to update the UI every time the app data changes because the observer does it for us.
+         */
         groupViewModel = ViewModelProvider(this).get(GroupViewModel::class.java)
         groupViewModel.getAllLanguageGroups.observe(viewLifecycleOwner, Observer { group ->
             adapter.setData(group)
